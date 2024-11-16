e083809025421adbbe65b479507dfa3b78484224
anme-22 <angeldavidmendez2012@gmail.com>
2024-11-16 11:39:20 -0600

    adding delete

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -39,14 +39,11 @@ export class CarsController {
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateCarDto: UpdateCarDto,
   ) {
-    return updateCarDto;
+    return this.carsService.update(id, updateCarDto);
   }
 
   @Delete(':id')
-  deleteCar(@Param('id', ParseIntPipe) id: string) {
-    return {
-      method: 'delete',
-      id,
-    };
+  deleteCar(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.delete(id);
   }
 }
